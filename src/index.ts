cc2f215c4d7c53b8221cea5831447e427a8da877
Lam Jun Rong <junron1@outlook.com>
2022-10-13 22:14:06 +0800

    Basic voting system

--- src/index.ts
+++ src/index.ts
@@ -21,40 +21,98 @@ const io = new Server(server);
 
 const game = new Chess();
 
-io.on("connection", (socket)=>{
+let votes: Map<string, number> | null = null;
+let nextVoteTime: number = 0;
+let votingRounds = 0;
+
+const votingTimeoutSeconds = 20;
+
+io.on("connection", (socket) => {
   console.log("New connection", socket.id)
 
-  socket.on("move", (move)=>{
-    if(!socket.data.role){
+  socket.on("vote", (move) => {
+    if (!socket.data.role) {
       socket.emit("error", "Select a role first");
       return;
     }
-    if(game.turn() != socket.data.role[0]){
+    if (game.turn() != socket.data.role[0]) {
       socket.emit("error", "Not your turn");
       return;
     }
     const result = game.move(move);
-    if(result == null){
+    if (result == null) {
       socket.emit("error", "Invalid move");
       return;
     }
-    socket.broadcast.emit("state", game.fen());
+    // We don't actually want to make that move yet
+    game.undo();
+    if (votes == null) {
+      return;
+    }
+    if (votes.has(move)) {
+      votes.set(move, votes.get(move)!! + 1)
+    } else {
+      votes.set(move, 1);
+    }
   })
 
-  socket.on("reset", ()=>{
-    game.reset()
-    io.emit("state", game.fen());
+  socket.on("reset", () => {
+    reset();
   })
 
-  socket.on("role", (role)=>{
-    if(!["white", "black"].includes(role)){
+  socket.on("role", (role) => {
+    if (!["white", "black"].includes(role)) {
       return socket.emit("error", "invalid role");
     }
     socket.data.role = role;
     console.log(socket.id, role);
-    socket.emit("state", game.fen());
+
+    if (votes == null) {
+      // new game
+      newVote();
+    }
+    socket.emit("state", {fen: game.fen(), nextVoteTime});
   })
 })
+
+function reset(){
+  game.reset()
+  votes = null;
+  io.emit("reset");
+  newVote();
+  io.emit("state", {fen: game.fen(), nextVoteTime});
+}
+
+function tallyVotes() {
+  if (votes == null) {
+    return;
+  }
+  console.log(`Voting round: ${votingRounds}`);
+  votingRounds++;
+  const sorted = Array.from(votes.entries()).sort((a, b) => {
+    const aNum = a[1];
+    const bNum = b[1];
+    return bNum - aNum;
+  }).slice(0, 10);
+
+  if (sorted.length == 0) {
+    // :skull:
+    io.emit("error", "No votes!");
+    reset();
+    return;
+  }
+
+  game.move(sorted[0][0]);
+  newVote();
+  io.emit("state", {fen: game.fen(), nextVoteTime});
+  io.emit("votes", sorted);
+}
+
+function newVote(){
+  votes = new Map<string, number>();
+  setTimeout(tallyVotes, 1000 * votingTimeoutSeconds);
+  nextVoteTime = new Date().getTime() + 1000 * votingTimeoutSeconds;
+}
 
 
 server.listen(3000, () => {
