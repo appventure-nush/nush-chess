3f0ac4195d6c7265d2efd7d0520b1b59c4877a8f
Lam Jun Rong <junron1@outlook.com>
2022-10-14 21:14:01 +0800

    Add voting updates

--- src/index.ts
+++ src/index.ts
@@ -22,10 +22,14 @@ const io = new Server(server);
 const game = new Chess();
 
 let votes: Map<string, number> | null = null;
+let votingTimeout: NodeJS.Timeout | null = null;
 let nextVoteTime: number = 0;
 let votingRounds = 0;
 
-const votingTimeoutSeconds = 20;
+const votingTimeoutSeconds = 60;
+
+let blacks = 0;
+let whites = 0;
 
 io.on("connection", (socket) => {
   console.log("New connection", socket.id)
@@ -54,6 +58,15 @@ io.on("connection", (socket) => {
     } else {
       votes.set(move, 1);
     }
+
+    const numVotes = sum(Array.from(votes.values()));
+    const players = game.turn() === 'w' ? whites : blacks;
+
+    io.emit("voting_update", {numVotes, players})
+
+    if (numVotes == players) {
+      tallyVotes();
+    }
   })
 
   socket.on("reset", () => {
@@ -66,16 +79,40 @@ io.on("connection", (socket) => {
     }
     socket.data.role = role;
     console.log(socket.id, role);
+    if (socket.data.role === "white") {
+      whites += 1;
+    } else if (socket.data.role === "black") {
+      blacks += 1;
+    }
 
     if (votes == null) {
       // new game
       newVote();
     }
     socket.emit("state", {fen: game.fen(), nextVoteTime});
+    const players = game.turn() === 'w' ? whites : blacks;
+    io.emit("voting_update", {numVotes: 0, players})
+  })
+
+  socket.on("disconnect", () => {
+    if (socket.data.role === "white") {
+      whites -= 1;
+    } else if (socket.data.role === "black") {
+      blacks -= 1;
+    }
   })
 })
 
-function reset(){
+function sum(arr: number[]) {
+  let res = 0;
+  for (const elem of arr) {
+    res += elem;
+  }
+  return res;
+}
+
+function reset() {
+  votingRounds = 0;
   game.reset()
   votes = null;
   io.emit("reset");
@@ -88,6 +125,7 @@ function tallyVotes() {
     return;
   }
   console.log(`Voting round: ${votingRounds}`);
+  console.log(votes);
   votingRounds++;
   const sorted = Array.from(votes.entries()).sort((a, b) => {
     const aNum = a[1];
@@ -106,11 +144,24 @@ function tallyVotes() {
   newVote();
   io.emit("state", {fen: game.fen(), nextVoteTime});
   io.emit("votes", sorted);
+  if(game.isCheckmate()){
+    // TODO: Start new game
+    if (votingTimeout != null) {
+      clearTimeout(votingTimeout);
+    }
+    return;
+  }
+  const players = game.turn() === 'w' ? whites : blacks;
+
+  io.emit("voting_update", {numVotes: 0, players})
 }
 
-function newVote(){
+function newVote() {
   votes = new Map<string, number>();
-  setTimeout(tallyVotes, 1000 * votingTimeoutSeconds);
+  if (votingTimeout != null) {
+    clearTimeout(votingTimeout);
+  }
+  votingTimeout = setTimeout(tallyVotes, 1000 * votingTimeoutSeconds);
   nextVoteTime = new Date().getTime() + 1000 * votingTimeoutSeconds;
 }
 
